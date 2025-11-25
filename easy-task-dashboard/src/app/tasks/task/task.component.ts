95a826315460396077cc47b73b660ed54ad7c1ec
Onur <akonur5834@gmail.com>
2025-11-25 21:42:41 +0300

    Update angular apps: add easy-task-dashboard and models version

---
+++ easy-task-dashboard/src/app/tasks/task/task.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, Input } from '@angular/core';
+import { TaskType } from './task.model';
+import { Card } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-task',
+  imports: [Card, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+
+export class Task {
+  @Input({required: true}) task!: TaskType;
+
+  private tasksService = inject(TasksService);
+
+  onCompleteTask(){
+    this.tasksService.removeTask(this.task.id);
+  }
+}
